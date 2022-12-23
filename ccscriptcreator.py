80b9a37413761014056c142b26ef0739a61c8fd4
mohamad hani janaty <66449213+hanicraft@users.noreply.github.com>
2022-12-24 00:21:20 +0330

    version 0.7

--- ccscriptcreator.py
+++ ccscriptcreator.py
@@ -14,8 +14,6 @@ class Widget1():
     def __init__(self, parent):
         self.gui(parent)
 
-    
-    
     def gui(self, parent):
         if parent == 0:
             self.w1 = Tk()
@@ -30,10 +28,8 @@ class Widget1():
         self.label1.place(x = 0, y = 550, width = 130, height = 32)
         self.output = Text(self.w1, font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
         self.output.place(x = 10, y = 10, width = 390, height = 370)
-        self.set_variable_name = Entry(self.w1,font=("Arial",12),bg="white")
-        self.set_variable_name.place(x = 420, y = 50, width = 100, height = 22)
         self.set_varible = Button(self.w1, text = "set varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-        self.set_varible.place(x = 520, y = 50, width = 140, height = 22)
+        self.set_varible.place(x = 420, y = 50, width = 140, height = 22)
         self.set_varible['command'] = self.setvar
         self.button1_copy = Button(self.w1, text = "set varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
         self.button1_copy.place(x = 1990, y = 360, width = 140, height = 22)
@@ -79,33 +75,33 @@ class Widget1():
         self.button12.place(x = 390, y = 420, width = 120, height = 22)
         self.button12['command'] = self.getcurrentnode
         self.label6 = Label(self.w1, text = "remember to format your code", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-        self.label6.place(x = 550, y = 510, width = 150, height = 22)
+        self.label6.place(x = 550, y = 490, width = 150, height = 22)
         self.label7 = Label(self.w1, text = "before adding it to coppercube", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-        self.label7.place(x = 550, y = 500, width = 150, height = 22)
+        self.label7.place(x = 550, y = 470, width = 150, height = 22)
         self.button13 = Button(self.w1, text = "export action", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
         self.button13.place(x = 610, y = 10, width = 90, height = 22)
         self.button13['command'] = self.save
         self.label8 = Label(self.w1, text = "remeber to add action_ to", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-        self.label8.place(x = 550, y = 520, width = 140, height = 22)
+        self.label8.place(x = 550, y = 510, width = 140, height = 22)
         self.label9 = Label(self.w1, text = "your file name so coppercube", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
         self.label9.place(x = 550, y = 530, width = 140, height = 22)
         self.label10 = Label(self.w1, text = "can reconize it", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-        self.label10.place(x = 550, y = 540, width = 90, height = 22)
+        self.label10.place(x = 550, y = 550, width = 90, height = 22)
+        self.varname = Entry(self.w1, font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.varname.place(x = 570, y = 50, width = 130, height = 22)
+        self.varname.insert(INSERT, "enter varible name")
+        self.createfunction = Button(self.w1, text = "create function", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.createfunction.place(x = 420, y = 140, width = 140, height = 22)
+        self.createfunction['command'] = self.func_create
+        self.funcname = Entry(self.w1, font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.funcname.place(x = 570, y = 140, width = 130, height = 22)
+        self.funcname.insert(INSERT, "enter function name")
 
-    # def popup(self):
-    #     popupWindow = Toplevel(self.w1)
-    #     popupWindow.title("setVariable")
-    #     popupWindow.geometry("200x100")
-    #     alert=Label(popupWindow, text="enter your variable name")
-    #     btn = Button(popupWindow, text="ok", command=popupWindow..3)
-    #     alert.pack()
-    #     btn.pack()
-    #     popupWindow.mainloop()
     def setvar(self):
-        self.output.insert('1.0', "\n ccbSetCopperCubeVariable("+self.set_variable_name.get()+", value of varible)")
+        self.output.insert('1.0', "\n ccbSetCopperCubeVariable("+ self.varname.get() +", value of varible)")
 
     def getvar(self):
-        self.output.insert('1.0', "\n ccbGetCopperCubeVariable(varible name)")
+        self.output.insert('1.0', "\n ccbGetCopperCubeVariable("+ self.varname.get() +")")
 
     def scene_switch(self):
         self.output.insert('1.0', "\n ccbSwitchToScene(sceneName)")
@@ -134,7 +130,6 @@ class Widget1():
     def setcamera(self):
         self.output.insert('1.0',"\n ccbGetActiveCamera()")
 
-
     def getcamera(self):
         self.output.insert('1.0',"\n ccbSetActiveCamera(cameraNode)")
 
@@ -144,81 +139,84 @@ class Widget1():
         with open(actionsave, "w") as fname3:
             fname3.write(outputtext)
 
+    def func_create(self):
+        self.output.insert('1.0', "function "+ self.funcname.get() +"() \n { \n \n }")
+
 if __name__ == '__main__':
     a = Widget1(0)
     a.w1.mainloop()
 
-# class Widget1():
-#     def __init__(self, parent):
-#         self.gui(parent)
+class Widget1():
+    def __init__(self, parent):
+        self.gui(parent)
 
-#     def gui(self, parent):
-#         if parent == 0:
-#             self.w1 = Tk()pip
-#             self.w1.configure(bg = '#666666')
-#             self.w1.geometry('710x580')
-#             self.w1.title("CopperCube Visual Scripting By hanicraft")
-#         else:
-#             self.w1 = Frame(parent)
-#             self.w1.configure(bg = '#666666')
-#             self.w1.place(x = 0, y = 0, width = 710, height = 580)
-#         self.label1 = Label(self.w1, text = "   Version 0.1 By HaniCraft", fg = "#2f2f2f", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.label1.place(x = 0, y = 550, width = 130, height = 32)
-#         self.output = Text(self.w1, font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.output.place(x = 10, y = 10, width = 390, height = 370)
-#         self.set_varible = Button(self.w1, text = "set varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.set_varible.place(x = 420, y = 50, width = 140, height = 22)
-#         self.button1_copy = Button(self.w1, text = "set varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.button1_copy.place(x = 1990, y = 360, width = 140, height = 22)
-#         self.get_varible = Button(self.w1, text = "get varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.get_varible.place(x = 420, y = 80, width = 140, height = 22)
-#         self.onkeydown = Button(self.w1, text = "on keydown", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.onkeydown.place(x = 60, y = 420, width = 140, height = 22)
-#         self.label2 = Label(self.w1, text = "Events", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.label2.place(x = 60, y = 390, width = 90, height = 22)
-#         self.onkeyup = Button(self.w1, text = "on keyup", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.onkeyup.place(x = 60, y = 450, width = 140, height = 22)
-#         self.onmousedown = Button(self.w1, text = "on mouse down", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.onmousedown.place(x = 60, y = 480, width = 140, height = 22)
-#         self.onmouseup = Button(self.w1, text = "on mouse up", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.onmouseup.place(x = 60, y = 510, width = 140, height = 22)
-#         self.label3 = Label(self.w1, text = "scene nodes", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.label3.place(x = 260, y = 390, width = 90, height = 22)
-#         self.button7 = Button(self.w1, text = "clone node", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.button7.place(x = 260, y = 420, width = 120, height = 22)
-#         self.button8 = Button(self.w1, text = "remove node", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.button8.place(x = 260, y = 450, width = 120, height = 22)
-#         self.label4 = Label(self.w1, text = "various", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.label4.place(x = 420, y = 20, width = 90, height = 22)
-#         self.label5 = Label(self.w1, text = "camera", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.label5.place(x = 260, y = 480, width = 90, height = 22)
-#         self.button9 = Button(self.w1, text = "set active camera", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.button9.place(x = 260, y = 510, width = 120, height = 22)
-#         self.button10 = Button(self.w1, text = "get active camera", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.button10.place(x = 260, y = 540, width = 120, height = 22)
-#         self.switch_scene = Button(self.w1, text = "switch scene", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.switch_scene.place(x = 420, y = 110, width = 140, height = 22)
-#         self.button12 = Button(self.w1, text = "get current node", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
-#         self.button12.place(x = 390, y = 420, width = 120, height = 22)
+    def gui(self, parent):
+        if parent == 0:
+            self.w1 = Tk()
+            self.w1.configure(bg = '#666666')
+            self.w1.geometry('710x580')
+            self.w1.title("CopperCube Visual Scripting By hanicraft")
+        else:
+            self.w1 = Frame(parent)
+            self.w1.configure(bg = '#666666')
+            self.w1.place(x = 0, y = 0, width = 710, height = 580)
+        self.label1 = Label(self.w1, text = "   Version 0.1 By HaniCraft", fg = "#2f2f2f", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.label1.place(x = 0, y = 550, width = 130, height = 32)
+        self.output = Text(self.w1, font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.output.place(x = 10, y = 10, width = 390, height = 370)
+        self.set_varible = Button(self.w1, text = "set varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.set_varible.place(x = 420, y = 50, width = 140, height = 22)
+        self.button1_copy = Button(self.w1, text = "set varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.button1_copy.place(x = 1990, y = 360, width = 140, height = 22)
+        self.get_varible = Button(self.w1, text = "get varible", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.get_varible.place(x = 420, y = 80, width = 140, height = 22)
+        self.onkeydown = Button(self.w1, text = "on keydown", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.onkeydown.place(x = 60, y = 420, width = 140, height = 22)
+        self.label2 = Label(self.w1, text = "Events", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.label2.place(x = 60, y = 390, width = 90, height = 22)
+        self.onkeyup = Button(self.w1, text = "on keyup", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.onkeyup.place(x = 60, y = 450, width = 140, height = 22)
+        self.onmousedown = Button(self.w1, text = "on mouse down", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.onmousedown.place(x = 60, y = 480, width = 140, height = 22)
+        self.onmouseup = Button(self.w1, text = "on mouse up", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.onmouseup.place(x = 60, y = 510, width = 140, height = 22)
+        self.label3 = Label(self.w1, text = "scene nodes", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.label3.place(x = 260, y = 390, width = 90, height = 22)
+        self.button7 = Button(self.w1, text = "clone node", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.button7.place(x = 260, y = 420, width = 120, height = 22)
+        self.button8 = Button(self.w1, text = "remove node", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.button8.place(x = 260, y = 450, width = 120, height = 22)
+        self.label4 = Label(self.w1, text = "various", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.label4.place(x = 420, y = 20, width = 90, height = 22)
+        self.label5 = Label(self.w1, text = "camera", fg = "#000000", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.label5.place(x = 260, y = 480, width = 90, height = 22)
+        self.button9 = Button(self.w1, text = "set active camera", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.button9.place(x = 260, y = 510, width = 120, height = 22)
+        self.button10 = Button(self.w1, text = "get active camera", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.button10.place(x = 260, y = 540, width = 120, height = 22)
+        self.switch_scene = Button(self.w1, text = "switch scene", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.switch_scene.place(x = 420, y = 110, width = 140, height = 22)
+        self.button12 = Button(self.w1, text = "get current node", font = tkinter.font.Font(family = "MS Shell Dlg 2", size = 8), cursor = "arrow", state = "normal")
+        self.button12.place(x = 390, y = 420, width = 120, height = 22)
 
         
-#     def setvar(self):
-#         self.set_varible.insert('1.0', "ccbSetCopperCubeVariable(varible name, value of varible)")
+    def setvar(self):
+        self.set_varible.insert('1.0', "ccbSetCopperCubeVariable(varible name, value of varible)")
 
-#     def getvar(self):
-#         self.get_varible.insert('1.0', "ccbGetCopperCubeVariable(varible name)")
+    def getvar(self):
+        self.get_varible.insert('1.0', "ccbGetCopperCubeVariable(varible name)")
     
-#     def scene_switch(self):
-#         self.switch_scene.insert('1.0', "ccbSwitchToScene(sceneName)")
+    def scene_switch(self):
+        self.switch_scene.insert('1.0', "ccbSwitchToScene(sceneName)")
 
-#     def keydown(self):
-#         self.onkeydown.insert('1.0', "ccbRegisterKeyDownEvent(keyPressedDown); function keyPressedDown(keyCode) {    print(A key was pressed down: + keyCode);}")
+    def keydown(self):
+        self.onkeydown.insert('1.0', "ccbRegisterKeyDownEvent(keyPressedDown); function keyPressedDown(keyCode) {    print(A key was pressed down: + keyCode);}")
 
-#     def keyup(self):
-#         insert('1.0', "ccbRegisterKeyUpEvent(keyPressedUp); function keyPressedUp(keyCode) {  print(A key was left up: + keyCode);}")
+    def keyup(self):
+        insert('1.0', "ccbRegisterKeyUpEvent(keyPressedUp); function keyPressedUp(keyCode) {  print(A key was left up: + keyCode);}")
 
-#     def mousedown(self):
-#         insert('1.0', "ccbRegisterMouseDownEvent(mousePressedDown); function mousePressedDown(button){   print(A mouse button was presssed down: + button);}")
+    def mousedown(self):
+        insert('1.0', "ccbRegisterMouseDownEvent(mousePressedDown); function mousePressedDown(button){   print(A mouse button was presssed down: + button);}")
 
-#     def mouseup(self):
-#         insert('1.0', "ccbRegisterMouseUpEvent(mousePressedUp); function mousePressedUp(button){   print(A mouse button was presssed up: + button);}")
+    def mouseup(self):
+        insert('1.0', "ccbRegisterMouseUpEvent(mousePressedUp); function mousePressedUp(button){   print(A mouse button was presssed up: + button);}")
